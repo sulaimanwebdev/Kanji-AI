0ba0e042619eb1f60daee72571170b4f13fb0af6
Muhammad Sulaiman <trendingofyt@gmail.com>
2023-04-30 20:35:16 +0500

    fonts added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-export default function App() {
+
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+
+function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <div >
+      <Router>
+         <Routes>
+         <Route exact path="/" element={<Login/>} />
+        
+         
+         </Routes>
+       </Router>
+    </div>
+  );
 }
+
+export default App;
