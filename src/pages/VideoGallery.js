6fbb5326a4a2364f2d0af1e86610f4566b31847c
Muhammad Sulaiman <trendingofyt@gmail.com>
2023-05-03 23:53:03 +0500

    popups added

--- src/pages/VideoGallery.js
+++ src/pages/VideoGallery.js
@@ -1,6 +1,7 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect } from "react";
 import SideBar from "../components/SideBar";
 import Navigation from "../components/Navigation";
+import VideoTable from "./VideoTable";
 
 const VideoGallery = () => {
    const [leftMenu, setleftMenu] = useState(true);
@@ -28,89 +29,6 @@ const VideoGallery = () => {
       window.removeEventListener("resize", handleResize);
     };
   }, []);
-
-
-
-
-  // for popup
-  const [popupVisible, setPopupVisible] = useState(false);
-  const popupRef = useRef(null);
-
-  useEffect(() => {
-    document.addEventListener("mousedown", handleClickOutside);
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
-  }, []);
-
-  const handleClickOutside = (event) => {
-    if (popupRef.current && !popupRef.current.contains(event.target)) {
-      setPopupVisible(false);
-    }
-  };
-
-  const togglePopup = () => {
-    setPopupVisible(!popupVisible);
-  };
-
-
-
-  const videos = [
-    {
-      video: 'https://youtube.com',
-      title: 'Tom Ford Makeup Tutorial: Effortless Glam',
-      views: "132K",
-      likes: "53K",
-      sentiment: 'Positive',
-      coreEmotion: 'Happy',
-      keywords: ['vulnerability', 'authenticity', 'shame'],
-    },
-
-    {
-      video: 'https://youtube.com',
-      title: 'Tom Ford Makeup Tutorial: Effortless Glam',
-      views: "132K",
-      likes: "53K",
-      sentiment: 'Positive',
-      coreEmotion: 'Happy',
-      keywords: ['vulnerability', 'authenticity', 'shame'],
-    },
-    
-    {
-      video: 'https://youtube.com',
-      title: 'Tom Ford Makeup Tutorial: Effortless Glam',
-      views: "132K",
-      likes: "53K",
-      sentiment: 'Positive',
-      coreEmotion: 'Happy',
-      keywords: ['vulnerability', 'authenticity', 'shame'],
-    },
-
-    {
-      video: 'https://youtube.com',
-      title: 'Tom Ford Makeup Tutorial: Effortless Glam',
-      views: "132K",
-      likes: "53K",
-      sentiment: 'Positive',
-      coreEmotion: 'Happy',
-      keywords: ['vulnerability', 'authenticity', 'shame'],
-    },
-
-    {
-      video: 'https://youtube.com',
-      title: 'Tom Ford Makeup Tutorial: Effortless Glam',
-      views: "132K",
-      likes: "53K",
-      sentiment: 'Positive',
-      coreEmotion: 'Happy',
-      keywords: ['vulnerability', 'authenticity', 'shame'],
-    },
-
-
-
-
-    // Add more videos here...
-  ];
 
 
   return (
@@ -159,63 +77,9 @@ const VideoGallery = () => {
 
             {/* main content */}
            <div className="mt-7">
-           <div className="cardShadow bg-white rounded-xl overflow-x-auto">
-      <table className="w-full border-collapse">
-        <thead className="bg-main text-white">
-          <tr className="text-left">
-            <th className="px-4 py-4 font-[500] whitespace-nowrap">Watch Video</th>
-            <th className="px-[40px] py-4 font-[500] whitespace-nowrap">Title</th>
 
-            <th className="relative px-[40px] py-4 font-[500] whitespace-nowrap">Views 
-            <button className="PopupButton inline self-center ml-2" onClick={togglePopup}>
-        <img src="/images/sort.svg" className="inline" alt="sort" />
-      </button>
+             <VideoTable/>
 
-      {popupVisible && (
-        <div
-          className="Popup dropDownShadow flex flex-col divide-y divide-[#E6E6E6] rounded-md bg-white px-4 py-2 absolute top-full right-0"
-          ref={popupRef}
-        >
-          <svg className="absolute bottom-[calc(100%-1px)] right-5" width="26" height="10" viewBox="0 0 35 19" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M34.5 18.5H0L16.5 0L34.5 18.5Z" fill="white"/>
-          </svg>
-          <div className="flex items-center justify-between gap-10 py-2">
-            <div className="text-[15px] text-black">Ascending</div>
-            <input type="checkbox" className="cursor-pointer w-[15px] h-[15px] accent-main" />
-          </div>
-
-          <div className="flex items-center justify-between gap-10 py-2">
-            <div className="text-[15px] text-black">Descending</div>
-            <input type="checkbox" className="cursor-pointer w-[15px] h-[15px] accent-main" />
-          </div>
-        </div>
-      )}
-
-            </th>
-
-            <th className="PopupButton px-[40px] py-4 font-[500] whitespace-nowrap">Likes <img src="/images/sort.svg" alt="sort" className="inline self-center ml-2" /></th>
-            <th className="PopupButton px-[40px] py-4 font-[500] whitespace-nowrap">Sentiment <img src="/images/bars.svg" alt="bars" className="inline self-center ml-2" /></th>
-            <th className="PopupButton px-[40px] py-4 font-[500] whitespace-nowrap">Core Emotion <img src="/images/bars.svg" alt="bars" className="inline self-center ml-2" /></th>
-            <th className="px-[40px] py-4 font-[500] whitespace-nowrap">Keywords</th>
-          </tr>
-        </thead>
-        <tbody className="font-[500] divide-y divide-[#C1C1C1]">
-          {videos.map((video, index) => (
-            <tr key={index} >
-              <td className="px-4 py-2.5 text-center whitespace-nowrap">
-                <a href={video.video} className="flex items-center justify-center w-[130px] h-[80px] bg-[#3A3A3A] rounded-lg"><img src="/images/play.svg" alt="play" /></a>
-              </td>
-              <td className="px-[40px] min-w-[300px]">{video.title}</td>
-              <td className="px-[40px] whitespace-nowrap">{video.views}</td>
-              <td className="px-[40px] whitespace-nowrap">{video.likes}</td>
-              <td className="px-[40px] whitespace-nowrap">{video.sentiment}</td>
-              <td className="px-[40px] whitespace-nowrap"><div className="px-5 pr-12 py-2 bg-[#FFE60029] font-[400] rounded-full flex items-center gap-3"><img src="/images/smiling-face.png" alt="smiling face" /> {video.coreEmotion}</div></td>
-              <td className="px-[40px] whitespace-nowrap"><button className="px-7 py-1.5 font-[400] text-main rounded-full border-solid border border-main transition hover:bg-main hover:text-white">Check Keywords</button></td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
            </div>
            
           </div>
